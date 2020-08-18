66d2c85fd74d63056b5fece39a94a3fdc4b8fc7a
amolgautam25 <amol_gautam25@yahoo.co.in>
2020-08-18 18:22:51 -0400

    distribute candies to people

--- Contains Duplicate II.cpp
+++ Contains Duplicate II.cpp
@@ -2,3 +2,25 @@
 // Created by amol_ on 18-Aug-2020.
 //
 
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int size= nums.size();
+        unordered_map<int,int> hash_map;
+
+        for (int i=0;i<size;i++)
+        {
+            if (hash_map.find(nums[i])!=hash_map.end())
+            {
+                //element is found
+                if(i-hash_map[nums[i]]<=k) return true;
+                else hash_map[nums[i]]=i;
+            }
+            else
+            {
+                hash_map[nums[i]]=i;
+            }
+        }
+        return false;
+    }
+};
--- distribute-candies-to-people.cpp
+++ distribute-candies-to-people.cpp
@@ -1,4 +1,41 @@
 //
 // Created by amol_ on 18-Aug-2020.
 //
+class Solution {
+public:
+    vector<int> distributeCandies(int candies, int num_people) {
 
+        vector<int> result(num_people);
+
+        int loop=0;
+        int current_candy=0;
+        int current_position=0;
+
+        while (candies>0){
+            if(current_position==num_people){
+                current_position=0;
+                loop++;
+            }
+
+            current_candy = (current_position+1)+(loop*num_people);
+            if (candies-current_candy < 0)
+            {
+                result[current_position]+=candies;
+            }
+            else{
+                result[current_position]+=current_candy;
+            }
+
+            current_position++;
+            candies=candies-current_candy;
+        }
+
+
+        //  cout<<"this is final answer"<<endl;
+        //  for (int i=0;i<result.size();i++)
+        //      cout<<result[i];
+
+        return result;
+
+    }
+};
